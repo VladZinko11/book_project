d9b563e38b36fc63477d5e5e0e294b18b0d5f33e
VladZinko <zinko1996@yandex.ru>
2024-09-15 21:32:54 +0300

    added annotation EnableAspectJAutoProxy in config AppContext

--- src/main/java/com/zinko/AppContext.java
+++ src/main/java/com/zinko/AppContext.java
@@ -9,11 +9,13 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 
 @Configuration
 @ComponentScan
+@EnableAspectJAutoProxy
 public class AppContext {
 
     @Bean
