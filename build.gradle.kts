331e16ba8b4204aef9ddf434b121b81e531dc72c
VladZinko <zinko1996@yandex.ru>
2024-11-19 14:29:02 +0300

    added dependencies for test container

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ val mapstructVersion = "1.6.2"
 val liquibaseVersion = "4.29.2"
 val hibernateVersion = "6.6.1.Final"
 val ehcacheVersion = "3.10.8"
+val testcontainersVersion = "1.20.3"
 
 tasks.withType<JavaExec> {
     systemProperty("file.encoding", "UTF-8")
@@ -32,8 +33,13 @@ tasks.withType<JavaExec> {
 
 dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
+    testImplementation ("org.testcontainers:junit-jupiter:$testcontainersVersion")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-log4j2")
@@ -42,9 +48,12 @@ dependencies {
     implementation("org.liquibase:liquibase-core:$liquibaseVersion")
     implementation("org.hibernate.orm:hibernate-jcache:$hibernateVersion")
     implementation("org.ehcache:ehcache:$ehcacheVersion")
+
     runtimeOnly("org.postgresql:postgresql")
+
     compileOnly("org.projectlombok:lombok")
     compileOnly("org.mapstruct:mapstruct:$mapstructVersion")
+
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
 }
